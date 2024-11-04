ecf159d8eed3e5e89478072f9097cb655cf0a0ea
seoh22 <seeunoh@Seui-MacBookPro-2.local>
2024-11-04 12:02:58 +0900

    database and login and register practice

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, redirect, url_for, session
+from database import DBhandler
+import hashlib
 import sys
 
 application = Flask(__name__)
+application.config["SECRET_KEY"] = "helloosp"
+
+DB = DBhandler()
 
 @application.route("/")
 def hello():
@@ -42,7 +47,29 @@ def reg_item_submit_post():
     image_file=request.files["file"]
     image_file.save("static/images/{}".format(image_file.filename))
     data=request.form
+    print(data)
+    DB.insert_item(data['name'], data, image_file.filename)
+    
     return render_template("submit_item_result.html", data=data, img_path="static/images/{}".format(image_file.filename))
 
+@application.route("/login")
+def login():
+    return render_template("login.html")
+
+@application.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+@application.route("/signup_post", methods=['POST'])
+def register_user():
+    data=request.form
+    pw=request.form['pw']
+    pw_hash = hashlib.sha256(pw.encode('utf-8')).hexdigest()
+    if DB.insert_user(data,pw_hash):
+        return render_template("login.html")
+    else:
+        flash("user id already exist!")
+        return render_template("signup.html")
+    
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
